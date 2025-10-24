2cd6744f6c48f2fda79a4872f2905f9cc6f76cf5
John-Steve-C <leonardchen701@outlook.com>
2025-10-24 22:23:48 +0000

    add_eval_readme

--- eval/run_eval.sh
+++ eval/run_eval.sh
@@ -1,4 +1,5 @@
 # evaluate based on doc_id
-python evaluate.py --runs_dir ../retrieve_results --qrels ../requests.jsonl
+python evaluate.py --runs_dir ../retrieve_results --qrels ../requests.jsonl --ndcg_k 2
+
 # evaluate based on doc content with LLM
 python evaluate_ragas.py --runs_path ../retrieve_results/retrieval_results_faiss.json --qrels ../requests.jsonl --model_name gpt-4.1-mini
